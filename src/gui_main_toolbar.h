24d1fffac6131f6eb843297de3d5632b15aff960
Ray <raysan5@gmail.com>
2022-10-15 18:55:52 +0200

    Update gui_main_toolbar.h

--- src/gui_main_toolbar.h
+++ src/gui_main_toolbar.h
@@ -1,6 +1,6 @@
 /*******************************************************************************************
 *
-*   MainToolbar v1.0.0 - Main Toolbar
+*   Main Toolbar
 *
 *   MODULE USAGE:
 *       #define GUI_MAIN_TOOLBAR_IMPLEMENTATION
@@ -9,13 +9,24 @@
 *       INIT: GuiMainToolbarState state = InitGuiMainToolbar();
 *       DRAW: GuiMainToolbar(&state);
 *
-*   LICENSE: Propietary License
+*   LICENSE: zlib/libpng
 *
-*   Copyright (c) 2019 raylib technologies (@raylibtech). All Rights Reserved.
+*   Copyright (c) 2019-2022 raylib technologies (@raylibtech).
 *
-*   Unauthorized copying of this file, via any medium is strictly prohibited
-*   This project is proprietary and confidential unless the owner allows
-*   usage in any other form by expresely written permission.
+*   This software is provided "as-is", without any express or implied warranty. In no event
+*   will the authors be held liable for any damages arising from the use of this software.
+*
+*   Permission is granted to anyone to use this software for any purpose, including commercial
+*   applications, and to alter it and redistribute it freely, subject to the following restrictions:
+*
+*     1. The origin of this software must not be misrepresented; you must not claim that you
+*     wrote the original software. If you use this software in a product, an acknowledgment
+*     in the product documentation would be appreciated but is not required.
+*
+*     2. Altered source versions must be plainly marked as such, and must not be misrepresented
+*     as being the original software.
+*
+*     3. This notice may not be removed or altered from any source distribution.
 *
 **********************************************************************************************/
 
@@ -27,33 +38,36 @@
 #define GUI_MAIN_TOOLBAR_H
 
 typedef struct {
-    Vector2 anchorMain;
-    Vector2 anchorZoom;
-    Vector2 anchorBack;
-    Vector2 anchorChannels;
-    Vector2 anchorHelpers;
-    Vector2 anchorEmpty;
-    Vector2 anchorFullscreen;
+
+    // Anchors for panels
+    Vector2 anchorFile;
+    Vector2 anchorEdit;
+    Vector2 anchorTools;
+    Vector2 anchorVisuals;
+    Vector2 anchorRight;
     
-    bool btnOpenPressed;
-    bool btnExportPressed;
-    bool showInfoWindowActive;
-    bool showEditWindowActive;
+    // File options
+    bool btnNewFilePressed;
+    bool btnLoadFilePressed;
+    bool btnSaveFilePressed;
+    bool btnExportFilePressed;
+    // Editor options
+
+    // Tool options
+
+    // Visual options
+    int visualStyleActive;
+    int prevVisualStyleActive;
+    int btnReloadStylePressed;
+    int languageActive;
+
+    // Help options
     bool btnHelpPressed;
     bool btnAboutPressed;
-    bool centerPressed;
-    float zoomValue;
-    int scaleFilterActive;
-    int channelsActive;
-    int backgroundActive;
-    bool helperGridActive;
-    bool helperCursorActive;
-    bool helperBoxActive;
+    bool btnSponsorPressed;
 
-    // Custom state variables (depend on development software)
-    // NOTE: This variables should be added manually if required
-    bool supportImage;
-    bool supportInfoEdit;
+    // Custom variables
+    // TODO.
 
 } GuiMainToolbarState;
 
@@ -106,102 +120,74 @@ GuiMainToolbarState InitGuiMainToolbar(void)
 {
     GuiMainToolbarState state = { 0 };
 
-    state.anchorMain = (Vector2){ 0, 0 };
-    state.anchorZoom = (Vector2){ 324, 0 };
-    state.anchorBack = (Vector2){ 723, 0 };
-    state.anchorChannels = (Vector2){ 882, 0 };
-    state.anchorHelpers = (Vector2){ 1081, 0 };
-    state.anchorEmpty = (Vector2){ 1080 + 150, 0 };
-    state.anchorFullscreen = (Vector2){ 0, 0 };
+    // Anchors for panels
+    state.anchorFile = (Vector2){ 0, 0 };
+    state.anchorEdit = (Vector2){ state.anchorFile.x + 160 - 1, 0 };
+    state.anchorTools = (Vector2){ state.anchorEdit.x + 188 - 1, 0 };
+    state.anchorVisuals = (Vector2){ 0, 0 };    // Anchor right, depends on screen width
+    state.anchorRight = (Vector2){ 0, 0 };      // Anchor right, depends on screen width
 
-    state.btnOpenPressed = false;
-    state.btnExportPressed = false;
-    state.showInfoWindowActive = true;
-    state.showEditWindowActive = true;
+    // Project/File options
+    state.btnNewFilePressed = false;
+    state.btnLoadFilePressed = false;
+    state.btnSaveFilePressed = false;
+    state.btnExportFilePressed = false;
+
+    // Edit options
+    //...
+
+    // Tool options
+    //...
+
+    // Visuals options
+    state.visualStyleActive = 0;
+    state.prevVisualStyleActive = 0;
+    state.languageActive = 0;
+
+    // Info options
     state.btnHelpPressed = false;
     state.btnAboutPressed = false;
-    state.centerPressed = false;
-    state.zoomValue = 1.0f;
-    state.scaleFilterActive = 0;
-    state.channelsActive = 0;
-    state.backgroundActive = 0;
-    state.helperGridActive = true;
-    state.helperCursorActive = true;
-    state.helperBoxActive = true;
+    state.btnSponsorPressed = false;
 
-    // Custom variables initialization
-    state.supportImage = false;
+    // Custom variables
+    // TODO.
 
     return state;
 }
 
 void GuiMainToolbar(GuiMainToolbarState *state)
 {
-    state->anchorFullscreen.x = (float)(GetScreenWidth() - 53);
-    
-    if (!state->supportImage) state->supportInfoEdit = false;
-    
-    GuiPanel((Rectangle){ state->anchorMain.x, state->anchorMain.y, 325, 48 });
-    state->btnOpenPressed = GuiButton((Rectangle){ state->anchorMain.x + 16, state->anchorMain.y + 12, 24, 24 }, "#1#"); 
+    // Toolbar panels
+    state->anchorRight.x = GetScreenWidth() - 104;       // Update right-anchor panel
+    state->anchorVisuals.x = state->anchorRight.x - 220 + 1;    // Update right-anchor panel
 
-    if (!state->supportInfoEdit) GuiDisable();
-    state->btnExportPressed = GuiButton((Rectangle){ state->anchorMain.x + 44, state->anchorMain.y + 12, 24, 24 }, "#7#");
-    state->showInfoWindowActive = GuiToggle((Rectangle){ state->anchorMain.x + 86, state->anchorMain.y + 13, 22, 22 }, "#15#", state->showInfoWindowActive);
-    state->showEditWindowActive = GuiToggle((Rectangle){ state->anchorMain.x + 110, state->anchorMain.y + 13, 22, 22 }, "#140#", state->showEditWindowActive);
-    GuiEnable();
-    
-    state->btnHelpPressed = GuiButton((Rectangle){ state->anchorMain.x + 154, state->anchorMain.y + 12, 75, 24 }, "#193#HELP"); 
-    state->btnAboutPressed = GuiButton((Rectangle){ state->anchorMain.x + 234, state->anchorMain.y + 12, 75, 24 }, "#191#ABOUT"); 
-    
-    GuiPanel((Rectangle){ state->anchorZoom.x, state->anchorZoom.y, 400, 48 });
-    GuiPanel((Rectangle){ state->anchorBack.x, state->anchorBack.y, 160, 48 });
-    GuiPanel((Rectangle){ state->anchorChannels.x, state->anchorChannels.y, 200, 48 });
-    GuiPanel((Rectangle){ state->anchorHelpers.x, state->anchorHelpers.y, 150, 48 });
+    GuiPanel((Rectangle){ state->anchorFile.x, state->anchorFile.y, 160, 40 }, NULL);
+    GuiPanel((Rectangle){ state->anchorEdit.x, state->anchorEdit.y, 188, 40 }, NULL);
+    GuiPanel((Rectangle){ state->anchorTools.x, state->anchorTools.y, state->anchorVisuals.x - state->anchorTools.x + 1, 40 }, NULL);
+    GuiPanel((Rectangle){ state->anchorVisuals.x, state->anchorVisuals.y, 220, 40 }, NULL);
+    GuiPanel((Rectangle){ state->anchorRight.x, state->anchorRight.y, 104, 40 }, NULL);
 
-    if ((GetScreenWidth() - state->anchorEmpty.x - 52) > 0)
-    {
-        GuiPanel((Rectangle){ state->anchorEmpty.x, state->anchorEmpty.y, GetScreenWidth() - state->anchorEmpty.x - 52, 48 });
-    }
+    // Project/File options
+    state->btnNewFilePressed = GuiButton((Rectangle){ state->anchorFile.x + 12, state->anchorFile.y + 8, 24, 24 }, "#8#");
+    state->btnLoadFilePressed = GuiButton((Rectangle){ state->anchorFile.x + 12 + 24 + 4, state->anchorFile.y + 8, 24, 24 }, "#5#");
+    state->btnSaveFilePressed = GuiButton((Rectangle){ state->anchorFile.x + 12 + 48 + 8, state->anchorFile.y + 8, 24, 24 }, "#6#");
+    state->btnExportFilePressed = GuiButton((Rectangle){ state->anchorFile.x + 12 + 72 + 12, state->anchorFile.y + 8, 24, 24 }, "#7#");
 
-    if (!state->supportImage) GuiDisable();
-    state->centerPressed = GuiButton((Rectangle){ state->anchorZoom.x + 16, state->anchorZoom.y + 12, 24, 24 }, "#105#");
-    state->zoomValue = GuiSliderBar((Rectangle){ state->anchorZoom.x + 86, state->anchorZoom.y + 19, 150, 12 }, "ZOOM:", TextFormat("%i%%", (int)(state->zoomValue*100.0f)), state->zoomValue, 0, 20.0f);
-    
-    int labelTextAlign = GuiGetStyle(LABEL, TEXT_ALIGNMENT);
-    GuiSetStyle(LABEL, TEXT_ALIGNMENT, TEXT_ALIGN_LEFT);
+    // Edit options
+    //...
+    // Tool options
+    //...
+    // Visuals options
+    GuiLabel((Rectangle){ state->anchorVisuals.x + 10, state->anchorVisuals.y + 8, 60, 24 }, "Style:");
+    GuiSetStyle(COMBOBOX, COMBO_BUTTON_WIDTH, 40);
+    state->visualStyleActive = GuiComboBox((Rectangle){ state->anchorVisuals.x + 8 + 48, state->anchorVisuals.y + 8, 120, 24 }, "Light;Jungle;Candy;Lavanda;Cyber;Terminal;Ashes;Bluish;Dark;Cherry;Sunny;Enefete", state->visualStyleActive);
+    GuiSetStyle(COMBOBOX, COMBO_BUTTON_WIDTH, 32);
+    state->btnReloadStylePressed = GuiButton((Rectangle){ state->anchorVisuals.x + 8 + 48 + 120 + 8, state->anchorVisuals.y + 8, 24, 24 }, "#76#");
 
-    state->scaleFilterActive = GuiToggleGroup((Rectangle){ state->anchorZoom.x + 340, state->anchorZoom.y + 14, 22, 22 }, "#34#;#35#", state->scaleFilterActive);
-    GuiLabel((Rectangle){ state->anchorZoom.x + 340 - MeasureTextEx(GuiGetFont(), "Filter:", (float)GuiGetStyle(DEFAULT, TEXT_SIZE), (float)GuiGetStyle(DEFAULT, TEXT_SPACING)).x - (float)GuiGetStyle(SLIDER, TEXT_PADDING), state->anchorZoom.y + 17, 72, 18 }, "Filter:");
-        
-    GuiLabel((Rectangle){ state->anchorBack.x + 16, state->anchorBack.y + 16, 32, 18 }, "Back:");
-    state->backgroundActive = GuiToggleGroup((Rectangle){ state->anchorBack.x + 50, state->anchorBack.y + 13, 22, 22 }, "#192#;;;", state->backgroundActive);
-    GuiLabel((Rectangle){ state->anchorChannels.x + 70 - MeasureTextEx(GuiGetFont(), "Channels:", (float)GuiGetStyle(DEFAULT, TEXT_SIZE), (float)GuiGetStyle(DEFAULT, TEXT_SPACING)).x - (float)GuiGetStyle(SLIDER, TEXT_PADDING), state->anchorChannels.y + 16, 56, 18 }, "Channels:");
-    state->channelsActive = GuiToggleGroup((Rectangle){ state->anchorChannels.x + 70, state->anchorChannels.y + 13, 22, 22 }, "*;R;G;B;A", state->channelsActive);
-    GuiEnable();
-    
-    GuiLabel((Rectangle){ state->anchorHelpers.x + 65 - MeasureTextEx(GuiGetFont(), "Helpers:", (float)GuiGetStyle(DEFAULT, TEXT_SIZE), (float)GuiGetStyle(DEFAULT, TEXT_SPACING)).x - (float)GuiGetStyle(SLIDER, TEXT_PADDING), state->anchorHelpers.y + 15, 50, 18 }, "Helpers:");
-    GuiSetStyle(LABEL, TEXT_ALIGNMENT, labelTextAlign);
-    
-    state->helperGridActive = GuiToggle((Rectangle){ state->anchorHelpers.x + 65, state->anchorHelpers.y + 13, 22, 22 }, "#97#", state->helperGridActive);
-    state->helperCursorActive = GuiToggle((Rectangle){ state->anchorHelpers.x + 89, state->anchorHelpers.y + 13, 22, 22 }, "#65#", state->helperCursorActive);
-    state->helperBoxActive = GuiToggle((Rectangle){ state->anchorHelpers.x + 113, state->anchorHelpers.y + 13, 22, 22 }, "#38#", state->helperBoxActive);
-    
-    GuiPanel((Rectangle){ state->anchorFullscreen.x, state->anchorFullscreen.y, 53, 48 });
-    state->fullScreenPressed = GuiButton((Rectangle){ state->anchorFullscreen.x + 15, state->anchorFullscreen.y + 12, 24, 24 }, "#53#");
-    
-    // Draw background colors
-    DrawRectangleRec((Rectangle){ state->anchorBack.x + 50 + 2*(22 + GuiGetStyle(TOGGLE, GROUP_PADDING)) + 1, state->anchorBack.y + 14, 20, 20 }, BLACK);
-    DrawRectangleRec((Rectangle){ state->anchorBack.x + 50 + 3*(22 + GuiGetStyle(TOGGLE, GROUP_PADDING)) + 1, state->anchorBack.y + 14, 20, 20 }, MAGENTA);
-
-    // NOTE: Using 3 pixels check size (18/6)
-    Rectangle bounds = { state->anchorBack.x + 50 + (23 + GuiGetStyle(TOGGLE, GROUP_PADDING)) + 1, state->anchorBack.y + 15, 18, 18 };
-    for (int y = 0; y < bounds.height/3; y++)
-    {
-        for (int x = 0; x <  bounds.width/3; x++)
-        {
-            DrawRectangle((int)bounds.x + x*3, (int)bounds.y + y*3, 3, 3, ((x + y)%2)? LIGHTGRAY : RAYWHITE);
-        }
-    }
+    // Info options
+    state->btnHelpPressed = GuiButton((Rectangle){ state->anchorRight.x + (GetScreenWidth() - state->anchorRight.x) - 12 - 72 - 8, state->anchorRight.y + 8, 24, 24 }, "#193#");
+    state->btnAboutPressed = GuiButton((Rectangle){ state->anchorRight.x + (GetScreenWidth() - state->anchorRight.x) - 12 - 48 - 4, state->anchorRight.y + 8, 24, 24 }, "#191#");
+    state->btnSponsorPressed = GuiButton((Rectangle){ state->anchorRight.x + (GetScreenWidth() - state->anchorRight.x) - 12 - 24, state->anchorRight.y + 8, 24, 24 }, "#186#");
 }
 
 #endif // GUI_MAIN_TOOLBAR_IMPLEMENTATION
