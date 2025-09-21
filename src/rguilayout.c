72d145fab39aa28dd4986b15be6dc996c1a5cfc9
Ray <raysan5@gmail.com>
2025-09-21 20:22:45 +0200

    Update tinyfiledialogs to v3.20

--- src/rguilayout.c
+++ src/rguilayout.c
@@ -80,7 +80,7 @@
 *       raylib 5.5-dev          - Windowing/input management and drawing
 *       raygui 4.5-dev          - Immediate-mode GUI controls with custom styling and icons
 *       rpng 1.5                - PNG chunks management
-*       tinyfiledialogs 3.19.1  - Open/save file dialogs, it requires linkage with comdlg32 and ole32 libs
+*       tinyfiledialogs 3.20    - Open/save file dialogs, it requires linkage with comdlg32 and ole32 libs
 *
 *   BUILDING:
 *     - Windows (MinGW-w64):
